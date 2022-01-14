7414e391208f8104eb839eb79f13eeb44e0e9b85
Patrick Ruan <patrick.ruan@Patricks-MacBook-Pro.local>
2022-01-14 10:50:49 -0500

    class react

--- app/javascript/components/InlineField.jsx
+++ app/javascript/components/InlineField.jsx
@@ -4,12 +4,19 @@ import ReactDOM from 'react-dom'
 const InlineField = ({ value, setValue }) => {
   const onChange = (event) => setValue(event.target.value);
 
+  const onKeyDown = (event) => {
+    if (event.key === "Enter" || event.key === "Escape") {
+      event.target.blur();
+    }
+  }
+
   return (
     <input
       type="text"
       aria-label="Field name"
       value={value}
       onChange={onChange}
+      onKeyDown={onKeyDown}
     />
   )
 }
--- app/javascript/components/TransformSection.jsx
+++ app/javascript/components/TransformSection.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom'
 import InlineField from './InlineField'
 
 class TransformSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+  }
+
   render() {
-    return <InlineField value={this.props.name} />;
+    return <InlineField value={this.state.value} />;
   }
 }
 
